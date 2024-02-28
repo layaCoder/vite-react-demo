9b9bd25517e28878bf7c80747549adc8b023cb88
liuchunfu <liuchunfu@fulu.com>
2024-02-28 17:24:46 +0800

    feat: 添加cart store/component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 
 import ProductList from './contaners/ProductList'
+import Cart from './contaners/Cart'
+import { useState } from 'react'
+
 
 import './App.css'
 
 function App() {
+    const [showCart,setShowCart] = useState(false)
+
 
   return (
       <div className="container">
           <div className="row">
-              <div className="col-md-12">
-                  <h1>React+Redux Shopping Cart Example</h1>
-              </div>
+                <h1>React+Redux Shopping Cart Example</h1>
           </div>
-          <div className="row">
-              <div className="col-md-8">
+          <button
+          onClick={()=>{
+            setShowCart(!showCart)
+          }}
+          >toggle cart
+          </button>
+            <div className="row" >
                   <ProductList />
-              </div>
-          </div>
-
+            </div>
+            <div>
+                {showCart&& <Cart />}
+            </div>
           <footer>
               <small>
                 react redux cart demo
